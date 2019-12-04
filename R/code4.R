a7de8be5943ba1b1c05cc8d9b3a7c6351d18a8bd
EdwinTh <edwinthoen@gmail.com>
2019-12-04 12:04:51 +0100

    completing 4

---
+++ R/code4.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+possible_range <- c(234208, 765869)
+all_nums <- possible_range[1]:possible_range[2]
+all_nums_splitted <- split_el(all_nums)
+
+## Part 1
+selection <- all_nums_splitted[map_lgl(all_nums_splitted, adjacent_criterium) &
+                    map_lgl(all_nums_splitted, increasing_criterium)]
+length(selection)
+
+split_el <- function(x) {
+  str_split(x, "") %>% 
+    map(as.numeric)
+}
+
+adjacent_criterium <- function(num_split) {
+  any(num_split[2:6] - num_split[1:5] == 0)
+}
+
+increasing_criterium <- function(num_split) {
+  all(num_split[2:6] - num_split[1:5] >= 0)
+}
+
+## Part 2
+has_two_adjacent <- function(x) {
+  paste(x[2:6] - x[1:5], collapse = "") %>% 
+    str_split("[1-9]") %>% 
+    map_lgl(~any(nchar(.x) == 1))
+}
+
+map_lgl(selection, has_two_adjacent) %>% sum
+
